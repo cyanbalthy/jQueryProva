13773fc64fcb784eab3a377189e0bc3a90f7dd59
lai2-01 <lai2-01@itismeucci.com>
2022-02-28 11:44:55 +0100

    aggiunta pulsante modifica

--- provuery.js
+++ provuery.js
@@ -1,4 +1,4 @@
-var data = [
+var data = /*$.ajax(method:"get", http://localhost:8080/swagger-ui.html, dataType:"json")*/[
                   {
                     "id": 10001,
                     "birthDate": "1953-09-01",
@@ -42,32 +42,35 @@ var data = [
 
                   }
                 ];
-
             var nextId = 10006;
-
             
 
             function updateEmployees() {
             var rows = "";
 
             $.each(data, function (key, value) {
-                  rows += "<tr>";
+                  rows += "<tr id='row-"+value.id+"'>";
+
                   rows += "<td>" + value.id + "</td>";
-                  rows += "<td>" + value.firstName + "</td>";
-                  rows += "<td>" + value.lastName + "</td>";
-                  rows += "<td>" + "<button class='delete-button' onclick='removeEmployee(" + value.id + "); updateEmployees();'>Cancella</button>" + "</td>";
+
+                  rows += "<td><span id='name-"+value.id+"'>" + value.firstName + "</span><input type='text' class='display-none' id='input-name-"+value.id+"' placeholder='"+value.firstName+"'></td>";
+
+                  rows += "<td><span id='lastname-"+value.id+"'>" + value.lastName + "</span><input type='text' class='display-none' id='input-lastname-"+value.id+"' placeholder='"+value.lastName+"'></td>";
+
+                  rows += "<td>" + "<button class='btn btn-primary' id='change-"+value.id+"' onclick='modify(" + value.id + ")'>Modifica</button>  "+
+                  "<button class='delete-button' id='"+value.id+"' onclick='removeEmployee(" + value.id + ")'>Cancella</button>" + "</td>";
                   rows += "</tr>";
                 });
                 $("#riempi-tab").html(rows);
             }
 
             function removeEmployee(id){
-              let i = 0;
               $.each(data, function(key, value){
                 if(value.id == id){
-                  data.splice(i, 1);
+                  data.splice(key, 1);
+                  $("#"+id).closest("tr").remove();
+                  return;
                 }
-                i++;
               })
             }
 
@@ -105,32 +108,58 @@ var data = [
               $("#hiring-date").val("");
             }
 
-            function modifyEmployee(id){
-              let i = 0;
-              $.each(data, function(key, value){
-                if(value.id == id){
-                  let f=value.id;
-                  data.splice(i, 1);
-                  data.push({
-                    "id": nextId,
-                    "birthDate": birth,
-                    "firstName": $("#name").val().trim(),
-                    "lastName": $("#lastname").val().trim(),
-                    "gender": gender,
-                    "hireDate": hiredate,
-                  })
-                }
-                i++;
-              })
+            function modify(id){
+              $("#name-"+id).addClass("display-none");
+              $("#input-name-"+id).removeClass("display-none");
+
+              $("#lastname-"+id).addClass("display-none");
+              $("#input-lastname-"+id).removeClass("display-none");
+
+              $("#change-"+id).removeClass("btn btn-primary");
+              $("#change-"+id).addClass("btn btn-success");
+              $("#change-"+id).text("salva");
+              $("#change-"+id).attr("onclick", "save_mod('"+id+"')");
             }
 
-            /*function saveModifiedModalInputs(){
-              addEmployee(
-                $("#name").val().trim(),
-                $("#lastname").val().trim(),
-                $("#birthday").val(),
-                $("#hiring-date").val(),
-                $("#gender").val()
-              );
-              updateEmployees();
-            }*/
+            function save_mod(id){
+              $("#name-"+id).removeClass("display-none");
+              $("#input-name-"+id).addClass("display-none");
+
+              $("#lastname-"+id).removeClass("display-none");
+              $("#input-lastname-"+id).addClass("display-none");
+
+              $("#change-"+id).removeClass("btn btn-success");
+              $("#change-"+id).addClass("btn btn-primary");
+              $("#change-"+id).text("modifica");
+              $("#change-"+id).attr("onclick", "modify('"+id+"')");
+
+              let newname=$("#input-name-"+id).val();
+              let newlastname=$("#input-lastname-"+id).val();
+
+              if(newname==""){
+                newname=$("#name-"+id).text();
+              }
+
+              if(newlastname==""){
+                newlastname=$("#lastname-"+id).text();
+              }
+
+              $("#name-"+id).text(newname);
+              $("#lastname-"+id).text(newlastname);
+              changeNames(newname, newlastname, id);
+
+              $("#input-name-"+id).attr("placeholder", newname);
+              $("#input-lastname-"+id).attr("placeholder", newlastname);
+
+              $("#input-name-"+id).val("");
+              $("#input-lastname-"+id).val("");
+            }
+
+            function changeNames(name, lastname, id){
+              $.each(data, function(key,value){
+                if(value.id==id){
+                  value.firstname=name;
+                  value.lastname=lastname;
+                }
+              })
+            }
